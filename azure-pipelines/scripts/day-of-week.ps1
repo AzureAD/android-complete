68059f1ac3dffc8daa8a346da572d374e11ea5cd
fadidurah <fadidurah@microsoft.com>
2023-12-03 23:28:25 -0500

    test wednesday

--- azure-pipelines/scripts/day-of-week.ps1
+++ azure-pipelines/scripts/day-of-week.ps1
@@ -6,4 +6,4 @@ $WeekDay = ( get-date ).DayOfWeek
 
 Write-Host "Setting $OutputVariable"
 Write-Host "Day of the week = $WeekDay"
-Write-Host "##vso[task.setvariable variable=$($OutputVariable)]Tuesday"
+Write-Host "##vso[task.setvariable variable=$($OutputVariable)]Wednesday"
